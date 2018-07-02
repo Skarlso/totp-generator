4efecbef17450b0001e8ab367246a6598a66722c
Gergely Brautigam <skarlso777@gmail.com>
2018-07-02 11:19:41 +0200

    TOTP Now does not use an environment property to store password making it secure to be used.

--- totp/filehandler.cpp
+++ totp/filehandler.cpp
@@ -11,56 +11,92 @@
 #include <stdio.h>
 #include <sstream>
 
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
+
 namespace fs = boost::filesystem;
 
-FileHandler::FileHandler() {
-    if(const char* env_p = std::getenv("TOTP_KEY")) {
-        std::string p(env_p);
-        password = p;
-    } else {
-        printf("Please set your environment property 'TOTP_KEY' to your totp password.\n");
-        exit(1);
-    }
+void SetStdinEcho(bool enable = true)
+{
+#ifdef WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
 
+    if (!enable)
+        mode &= ~ENABLE_ECHO_INPUT;
+    else
+        mode |= ENABLE_ECHO_INPUT;
+
+    SetConsoleMode(hStdin, mode);
+
+#else
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (!enable)
+        tty.c_lflag &= ~ECHO;
+    else
+        tty.c_lflag |= ECHO;
+
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+#endif
+}
+
+FileHandler::FileHandler()
+{
+    SetStdinEcho(false);
+    std::cerr << "password: ";
+    std::cin >> password;
+    SetStdinEcho(true);
     const char *homedir;
 
-    if ((homedir = getenv("HOME")) == NULL) {
+    if ((homedir = getenv("HOME")) == NULL)
+    {
         homedir = getpwuid(getuid())->pw_dir;
     }
     fs::path home;
     home /= std::string(homedir);
     home /= CONFIG_FOLDER;
     struct stat buffer;
-    if (stat (home.c_str(), &buffer) != 0) {
+    if (stat(home.c_str(), &buffer) != 0)
+    {
         fs::create_directory(home);
     }
     home /= CONFIG_FILE;
-    if (stat (home.c_str(), &buffer) != 0) {
+    if (stat(home.c_str(), &buffer) != 0)
+    {
         std::string content = "";
         Encrypt::Encrypt *encrypt = new Encrypt::Encrypt();
         std::string encryptedContent = encrypt->encrypt(content, password);
-        std::ofstream outfile (home.c_str());
+        std::ofstream outfile(home.c_str());
         outfile << encryptedContent;
         outfile.close();
     }
     configLocation = home;
 }
 
-void FileHandler::saveAccountFile(const std::map<std::string, std::string> accountsToSave) {
+void FileHandler::saveAccountFile(const std::map<std::string, std::string> accountsToSave)
+{
     std::ostringstream content;
-    for (auto it = accountsToSave.begin(); it != accountsToSave.end(); ++it) {
+    for (auto it = accountsToSave.begin(); it != accountsToSave.end(); ++it)
+    {
         content << it->first << ":" << it->second << '\n';
     }
 
     Encrypt::Encrypt *encrypt = new Encrypt::Encrypt();
     std::string encryptedContent = encrypt->encrypt(content.str(), password);
 
-    std::ofstream accFile (configLocation.c_str());
+    std::ofstream accFile(configLocation.c_str());
     accFile << encryptedContent;
     accFile.close();
 }
 
-std::map<std::string, std::string> FileHandler::loadAccountFile() {
+std::map<std::string, std::string> FileHandler::loadAccountFile()
+{
     std::map<std::string, std::string> accounts;
     std::ifstream file(configLocation.c_str());
     std::stringstream buffer;
@@ -71,7 +107,8 @@ std::map<std::string, std::string> FileHandler::loadAccountFile() {
     std::istringstream stream;
     stream.str(decrypted);
     std::string str;
-    while (std::getline(stream, str)) {
+    while (std::getline(stream, str))
+    {
         char account[1024];
         char token[1024];
         std::sscanf(str.c_str(), "%[^:]:%s", account, token);
